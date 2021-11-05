4945a9fa6e54fae3a0a0405af4c29a68b3e8033d
myounis789 <mohammedyounis789@gmail.com>
2021-11-05 12:04:03 +0000

    'Corrected fixes in main.py'

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
 from application import app
 
-application=Flask(__name__)
-application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-application.config['SECRET_KEY'] = os.getenv('secretkey')
-db = SQLAlchemy(application)
-
 app.run(debug=True, host = '0.0.0.0')
 
 
